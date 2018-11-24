19a5dca0e0366c894df15f2a1cb723a970c1cadb
Mohammed Sadiq <sadiqmmm@gmail.com>
2018-11-24 16:40:02 +0530

    Working cal

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Calculator'),
-      
     );
   }
 }
@@ -27,12 +26,70 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String output = "0";
+
+  String _output = "0";
+  double num1 = 0.0;
+  double num2 = 0.0;
+  String operand = "";
+
+  buttonPressed(String btnText) {
+    if (btnText == "CLEAR") {
+      _output = "0";
+      num1 = 0.0;
+      num2 = 0.0;
+      operand = "";
+    } else if (btnText == "+" ||
+        btnText == "-" ||
+        btnText == "/" ||
+        btnText == "x") {
+      num1 = double.parse(output);
+      operand = btnText;
+      _output = "0";
+    } else if (btnText == ".") {
+      if (_output.contains(".")) {
+        print("Already contains a decimals");
+        return;
+      } else {
+        _output = _output + btnText;
+      }
+    } else if (btnText == "=") {
+      num2 = double.parse(output);
+
+      if (operand == "+") {
+        _output = (num1 + num2).toString();
+      }
+      if (operand == "-") {
+        _output = (num1 - num2).toString();
+      }
+      if (operand == "x") {
+        _output = (num1 * num2).toString();
+      }
+      if (operand == "/") {
+        _output = (num1 / num2).toString();
+      }
+
+      num1 = 0.0;
+      num2 = 0.0;
+      operand = "";
+    } else {
+      _output = _output + btnText;
+    }
+    print(_output);
+    setState(() {
+      output = double.parse(_output).toStringAsFixed(2);
+    });
+  }
+
   Widget buildButton(String btnText) {
     return Expanded(
       child: OutlineButton(
-        padding: EdgeInsets.all(24.0),
-        child: Text(btnText),
-        onPressed: () => {},        
+        padding: EdgeInsets.all(20.0),
+        child: Text(
+          btnText,
+          style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+        ),
+        onPressed: () => buttonPressed(btnText),
       ),
     );
   }
@@ -47,7 +104,15 @@ class _MyHomePageState extends State<MyHomePage> {
         body: Container(
           child: Column(
             children: <Widget>[
-              Text("0"),
+              Container(
+                  alignment: Alignment.centerRight,
+                  padding:
+                      EdgeInsets.symmetric(vertical: 24.0, horizontal: 12.0),
+                  child: Text(
+                    output,
+                    style:
+                        TextStyle(fontSize: 48.0, fontWeight: FontWeight.bold),
+                  )),
               Expanded(
                 child: Divider(),
               ),
@@ -79,7 +144,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   ]),
                   Row(children: [
                     buildButton("CLEAR"),
-                    buildButton("="),                    
+                    buildButton("="),
                   ]),
                 ],
               ),
